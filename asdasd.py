2e3d51a93fa9cc0f524b26069189289348900e8d
rahuljhajhariya <jhajhariyaaniket@gmail.com>
2026-04-23 11:56:26 +0530

    third commit

--- asdasd.py
+++ asdasd.py
@@ -10,3 +10,42 @@ if(len(pas)>0):
             print("not a valid password")
 else:
     print("not a valid password")
+
+pas="reg123"
+
+if(len(pas)>0):
+    for i in range(len(pas)):
+        if(pas[i] in "qwertyuiopasdfghjklzxcvbnm" ):
+            if(pas[i] in "123456789"):
+                print("valid password")
+                break
+        else:
+            print("not a valid password")
+else:
+    print("not a valid password")
+
+pas="reg123"
+
+if(len(pas)>0):
+    for i in range(len(pas)):
+        if(pas[i] in "qwertyuiopasdfghjklzxcvbnm" ):
+            if(pas[i] in "123456789"):
+                print("valid password")
+                break
+        else:
+            print("not a valid password")
+else:
+    print("not a valid password")
+
+pas="reg123"
+
+if(len(pas)>0):
+    for i in range(len(pas)):
+        if(pas[i] in "qwertyuiopasdfghjklzxcvbnm" ):
+            if(pas[i] in "123456789"):
+                print("valid password")
+                break
+        else:
+            print("not a valid password")
+else:
+    print("not a valid password")
