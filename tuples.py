66f2fccc9ac9f113914f468a73619cbaaeebc2dd
rahuljhajhariya <jhajhariyaaniket@gmail.com>
2026-05-10 14:51:24 +0530

    assignment

---
+++ tuples.py
@@ -0,0 +1,16 @@
+'''
+are the datatype same as list
+list = mutable
+tuples = immutable
+
+difference between list and tuples
+why list is slower than tuple
+what is fall by value and fall by reference
+what is update methord 
+what is pop item 
+'''
+
+
+ticket={132:"aman",6549:"rahul"}
+print(ticket)
+print(type(ticket))
